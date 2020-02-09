7d4a2871829b4a171fbb0652caafb7861c6d47b5
Zoltan Palyi <palyi@users.noreply.github.com>
2020-02-09 01:45:34 +0100

    Add files via upload

---
+++ Navigation.py
@@ -0,0 +1,98 @@
+from unityagents import UnityEnvironment
+import numpy as np
+import random
+import torch
+from collections import deque
+import matplotlib.pyplot as plt
+from agent import Agent #reference to the agent.py include file
+
+env = UnityEnvironment(file_name="e:\\Development\\DeepReinf\\DQN2-Bananas\Banana.exe")
+
+brain_name = env.brain_names[0]
+brain = env.brains[brain_name]
+env_info = env.reset(train_mode=True)[brain_name]
+
+print('Number of agents:', len(env_info.agents))
+action_size = brain.vector_action_space_size
+print('Number of actions:', action_size)
+state = env_info.vector_observations[0]
+print('States look like:', state)
+state_size = len(state)
+print('States have length:', state_size)
+
+env_info = env.reset(train_mode=True)[brain_name] # reset the environment
+state = env_info.vector_observations[0]            # get the current state
+score = 0                                          # initialize the score
+while True:
+    action = np.random.randint(action_size)        # select an action
+    env_info = env.step(action)[brain_name]        # send the action to the environment
+    next_state = env_info.vector_observations[0]   # get the next state
+    reward = env_info.rewards[0]                   # get the reward
+    done = env_info.local_done[0]                  # see if episode has finished
+    score += reward                                # update the score
+    state = next_state                             # roll over the state to next time step
+    if done:                                       # exit loop if episode finished
+        break
+    
+print("Score: {}".format(score))
+
+
+
+
+def dqn(n_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.995, train_mode=True):
+    """Deep Q-Learning.
+    
+    Params
+    ======
+        n_episodes (int): maximum number of training episodes
+        max_t (int): maximum number of timesteps per episode
+        eps_start (float): starting value of epsilon, for epsilon-greedy action selection
+        eps_end (float): minimum value of epsilon
+        eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
+	train_mode (bool): 'True' if training mode is on
+    """
+    scores = []                        # list containing scores from each episode
+    scores_window = deque(maxlen=100)  # last 100 scores
+    eps = eps_start                    # initialize epsilon
+    achievements = []		       # tracking achievement history of agent (must get an average score of +13 over 100 consecutive episodes)
+    for i_episode in range(1, n_episodes+1):
+        env_info = env.reset(train_mode=train_mode)[brain_name]
+        state = env_info.vector_observations[0]
+        score = 0
+        for t in range(max_t):
+            action = agent.act(state, eps).astype(int) #IT DOES NOT WORK WITHOUT TYPE CASTING !!!!
+            env_info=env.step(action)[brain_name]
+            next_state=env_info.vector_observations[0] 
+            reward=env_info.rewards[0]
+            done=env_info.local_done[0] 
+            agent.step(state, action, reward, next_state, done)
+            state = next_state
+            score += reward
+            if done:
+                break 
+        scores_window.append(score)       # save most recent score
+        scores.append(score)              # save most recent score
+        achievement=np.mean(scores_window) # retrieve value of this particular achievement
+        achievements.append(achievement)  # append this particular achievement to the history
+        eps = max(eps_end, eps_decay*eps) # decrease epsilon
+        print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
+        if i_episode % 100 == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+        if achievement>=13:               # if environment solved according to task definition
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
+            if train_mode:
+                torch.save(agent.qnetwork_local.state_dict(), 'e:\\Development\\DeepReinf\\DQN2-Bananas\checkpoint.pth')
+                print('Train mode was on, Q network written in file.')    
+            break
+    return scores
+
+agent = Agent(state_size=state_size, action_size=action_size, seed=0)
+scores = dqn(n_episodes=500, eps_decay=0.98, eps_end=0.02, train_mode=True)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.plot(np.arange(len(scores)), scores)
+plt.ylabel('Score')
+plt.xlabel('Episode #')
+plt.show()
+env.close
